77dcceaaaf630190e387f6be64918b9b0db23dd1
MichaelLim-QMTFLIM <qmtflim@tip.edu.ph>
2025-09-29 21:55:08 +0800

    Update: Introduction Scene
    Added:
    - Intro Sequence (New Scene)
    - Added 7 intro slides
    - Added Script to connect Intro to TitleScreen and Level1
    
    - Changed a few Scripts in LevelMenu.cs

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class LevelMenu : MonoBehaviour
 {
     public void OpenLevel(int levelId)
     {
-        string levelName = "Level" + levelId;
-        SceneManager.LoadScene(levelName);
+        if (levelId == 1) 
+        {
+            SceneManager.LoadScene("Intro"); 
+        }
+        else
+        {
+            string levelName = "Level" + levelId;
+            SceneManager.LoadScene(levelName);
+        }
     }
 }
