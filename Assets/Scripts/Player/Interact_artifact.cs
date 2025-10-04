cd869b6930583b5b828f6e5063574ee7635c98e7
MichaelLim-QMTFLIM <qmtflim@tip.edu.ph>
2025-10-04 22:12:40 +0800

    Update: (Panels) 90%
    4/10/2025:
    
    Added:
    - Images for Panels (still temporary/placeholders panel)
    - X Button
    
    Changes:
    - Updated Input System to 1.14.2
    - Changes to the Interact_artifacts.cs
    
    *Done in Coding and Logic, still waiting for the Final Info Panels to replace placeholder Panels

--- Assets/Scripts/Player/Interact_artifact.cs
+++ Assets/Scripts/Player/Interact_artifact.cs
@@ -1,52 +1,80 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interact_artifact : MonoBehaviour
 {
     public GameObject[] artefacts;
     public GameObject[] artefact_Panels;
-    public GameObject[] Interact_UI; 
+    public GameObject[] Interact_UI;
+    public GameObject X_Button;
+
     void Start()
     {
         gameObject.SetActive(true);
 
-        for (int x = 0; x < artefacts.Length; x++) {
+        for (int x = 0; x < artefacts.Length; x++)
+        {
             Interact_UI[x].SetActive(false);
+            artefact_Panels[x].SetActive(false);
         }
 
-        
+
     }
 
-    public void OnTriggerEnter2D (Collider2D other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         for (int i = 0; i < artefacts.Length; i++)
         {
-            if (other.gameObject ==  artefacts[i])
+            if (other.gameObject == artefacts[i])
             {
                 Interact_UI[i].SetActive(true);
                 Debug.Log(artefacts[i].name);
-                
-                
-                
-            } 
-            
-        }
-    }
-    
-    public void OnTriggerExit2D (Collider2D other)
-    {
-        for (int i = 0; i < artefacts.Length; i++)
-        {
-            if (other.gameObject ==  artefacts[i])
-            {
-                Interact_UI[i].SetActive(false);
-                Debug.Log("Exit");
-                
-                
-                
-            } 
-            
+            }
+
         }
     }
 
+    void OnTriggerStay2D(Collider2D other)
+    {
+        for (int i = 0; i < artefacts.Length; i++)
+        {
+            if (other.gameObject == artefacts[i])
+            {
+                Interact_UI[i].SetActive(true);
+
+                if (Keyboard.current.fKey.wasPressedThisFrame)
+                {
+                    Debug.Log("Pressed F and Paused: " + artefacts[i].name);
+                    artefact_Panels[i].SetActive(true);
+                    X_Button.SetActive(true);
+                    Time.timeScale = 0f; // pause
+                }
+            }
+        }
+    }
+
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        for (int i = 0; i < artefacts.Length; i++)
+        {
+            if (other.gameObject == artefacts[i])
+            {
+                Interact_UI[i].SetActive(false);
+                Debug.Log("Exit");
+
+            }
+
+        }
+    }
+
+    void Update()
+    {
+        if (!X_Button.activeSelf)
+        {
+            Debug.Log("Continue");
+            Time.timeScale = 1f;
+        }
+    }
 
 }
